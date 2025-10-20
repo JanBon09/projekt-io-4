1249cc07dab31a5db37047ef6c12e6c19d3dc0c2
adamowskipl <matek0402@gmail.com>
2025-10-20 22:11:03 +0200

    test client

---
+++ server/sockets/websockets.js
@@ -0,0 +1,38 @@
+import { server } from "../src/index.js";
+import { Server as IOServer } from "socket.io";
+
+const io = new IOServer(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+let nicknames = {};
+
+io.on("connection", (socket) => {
+    console.log("New client connected:", socket.id, socket.handshake.address);
+    socket.emit("welcome", { message: "connected", socketId: socket.id });
+
+    socket.on("create-nickname", (nick) => {
+        socket.nickname = nick;
+        nicknames[socket.id] = nick;
+        console.log("Nickname created:", socket.id, nick);
+        io.emit("nicknames", nicknames);
+    });
+
+    socket.on("message", (data) => {
+        console.log("message from", socket.id, data);
+        socket.emit("echo", data);
+    });
+
+    socket.on("disconnect", (reason) => {
+        console.log("Socket disconnected:", socket.id, reason);
+        delete nicknames[socket.id];
+        io.emit("nicknames", nicknames);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("WebSocket server is running on port 3000");
+});
