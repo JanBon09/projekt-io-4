d3efc105f28f06462f0123c82fce04ce287ba344
Beyshin <thedemexus@gmail.com>
2025-10-20 22:17:12 +0200

    mini changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import { io } from "socket.io-client";
+import './App.css';
 
 const openWebsocket = () => {
     const socket = io("http://localhost:3000");
@@ -12,11 +13,8 @@ const openWebsocket = () => {
 function App() {
     const [page, setPage] = useState("default")
 
-
-
   return (
     <div className="App">
-        <h1>test</h1>
         {page === "default" && (
             <>
             <h1>Projekt IO-4</h1>
@@ -25,12 +23,12 @@ function App() {
         )}
 
         {page === "login" && (
-            <>
+            <div className="login-block">
                 <h1>Logowanie</h1>
                 <input type="text"></input>
                 <button onClick={() => openWebsocket()}>Login</button>
                 <button onClick={() => setPage("default")}>Home</button>
-            </>
+            </div>
         )}
 
 
