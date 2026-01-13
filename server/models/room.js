7918b2d27d3d81502b0faee8a08852de832b5f74
Beyshin <thedemexus@gmail.com>
2026-01-13 17:07:52 +0100

    added password hint functionality

--- server/models/room.js
+++ server/models/room.js
@@ -2,19 +2,19 @@ export class Room {
     constructor(id, name, players, ownerId) {
         this.id = id;
         this.name = name;
-        this.players = players; // Tablica obiektów graczy
-        this.ownerId = ownerId; // ID socketa właściciela
+        this.players = players;
+        this.ownerId = ownerId;
 
-        // Pola gry
+
         this.currentAnswer = "";
         this.drawingPlayerId = null;
         this.round = 0;
-        this.totalRounds = 5; // Domyślnie, nadpisywane przy starcie
+        this.totalRounds = 5;
         this.isGameStarted = false;
+        this.currentHint = "";
 
-        // Timer
         this.timerInterval = null;
         this.timeLeft = 0;
-        this.solvedBy = []; // Lista ID graczy, którzy zgadli w danej rundzie
+        this.solvedBy = [];
     }
 }
